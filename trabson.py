fb362f4c2ef9a61f6e4d69b88455a18e5e22ef86
Cleiton <cleiton_biou@hotmail.com>
2019-10-26 03:19:25 -0300

    Comando entrar

--- trabson.py
+++ trabson.py
@@ -3,6 +3,7 @@ import bd1
 def criarconta():
     cont = 0 
     while cont == 0:
+        telefone = 0
         usuario = input('Insira seu usuario: ')
         senha = input('Insira sua senha: ')
         email = input('Insira um e-mail válido: ')
@@ -35,7 +36,7 @@ def criarconta():
             if row[1] > 0:
                 print('ERROR: Nick já existe')      
         else: 
-            bd1.cursor.execute("insert into usuarios(email,usuario,nick,datanasc,senha) values(%s,%s,%s,%s,%s)",(email,usuario,nick,datanasc,senha))
+            bd1.cursor.execute("insert into usuarios(telefone,email,usuario,nick,datanasc,senha) values(%s,%s,%s,%s,%s,%s)",(telefone,email,usuario,nick,datanasc,senha))
             bd1.connection.commit()
             print('Úsuario cadastrado com sucesso')
             cont = 1 
@@ -44,18 +45,21 @@ def criarconta():
 
 
 def Entrar():
+    
+    usuario = input('Úsuario: ')
+    senha = input('Senha: ')
     pop = 0
     while pop == 0:
-        usuario = input('Úsuario: ')
-        senha = input('Senha: ')
 
         bd1.cursor.execute("SELECT usuario,senha FROM usuarios WHERE usuario=%s AND senha=%s",(usuario, senha))
         numRow = bd1.cursor.rowcount
-        
+        print('-> Sair\n-> Cadastrar time\n-> Times registrados\n-> Configurações\n-> Vincular telefone')
+        ops = input()
         if numRow > 0:
             #usuário logado
-            print('-> Sair\n-> Cadastrar time\n-> Times registrados\n-> Configurações\n-> Vincular telefone')
-            ops = input()
+            
+            
+        
             # if ops == 'configurações':
             #     bd1.cursor.execute("SELECT email FROM usuarios WHERE email = %s",(recebeEmail,))
             #     emailBD = bd1.cursor.fetchall()
@@ -65,16 +69,22 @@ def Entrar():
                 back = 0
                 while back == 0:
                     tel = input('Digite o número [+DDD][8212345678] -> +')
-        
-                    if tel.isnumeric() == True:
-                     
-                        
-                        row = bd1.cursor.rowcount
-                        if row[0]:
-                            bd1.cursor.execute(f"insert into usuarios(telefone) values({tel})")
+                    
+                    if tel.isnumeric() == True: 
+                        bd1.cursor.execute("select telefone from usuarios where usuario = %s",(usuario,))  
+                        pegar = bd1.cursor.fetchall()
+                        if pegar[0] == 0:
+                            bd1.cursor.execute("update usuarios set telefone = %s where usuario = %s",(tel,usuario,))
                             bd1.connection.commit()
                             print('Vinculado com sucesso')
-                            back = 1
+                            pop = 1
+                            
+                        else:
+                            bd1.cursor.execute("select telefone from usuarios where usuario = %s",(usuario,))  
+                            bdTelefone = bd1.cursor.fetchall()
+                            
+                            print(f'Conta já vinculada com telefone {bdTelefone[0]}') 
+                            back = 1                 
                     else:
                         print('Apenas números!')
                         continue

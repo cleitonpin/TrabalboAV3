6104c54f00d031ced432db77b5c386366d9b3bd2
Cleiton <cleiton_biou@hotmail.com>
2019-10-27 00:18:37 -0300

    mudanças

--- testes.py
+++ testes.py
@@ -1,13 +1,18 @@
+import random
 
-back = 0
-while back == 0:
-    tel = input(f'Digite o número [+DDD][8212345678] -> +')
+pos = 0
+while pos == 0:
+    codigo_campeonato = random.randrange(1001, 9999)
+    cod_Campeonato = input(f'Insira o código do campeonato [{codigo_campeonato}] -> ')
 
-    print (tel.isnumeric())
-    
-    if tel.isnumeric() == True:
-        print('Vinculado com sucesso')
-        back = 1
+    if cod_Campeonato.iscn== True:
+        if cod_Campeonato != codigo_campeonato:
+            print('Código errado, digite novamente')
+            continue
+        elif cod_Campeonato == codigo_campeonato:
+            print('Correto')
+            pos = 1
     else:
         print('Apenas números!')
         continue
+
--- trabson.py
+++ trabson.py
@@ -1,4 +1,5 @@
 import bd1
+import random
 
 def criarconta():
     cont = 0 
@@ -18,10 +19,10 @@ def criarconta():
         if verificaremail[1] == -1:
             print("\n\nErro: email inválido [ausência de: '.com/.com.br/...']\n")
             continue
-        if len(usuario) <= 4:
-            print('Número de caracteres insuficiente[minimo 4]\n')
+        if len(usuario) < 4:
+            print('Número de caracteres insuficiente[mínimo 4]\n')
             continue
-        elif len(usuario) >= 12:
+        elif len(usuario) > 12:
             print('Número de caracteres acima do indicado[máximo de 12]\n')
             continue
         bd1.cursor.execute("select usuario from usuarios where usuario = %s", (usuario,))
@@ -41,7 +42,43 @@ def criarconta():
             print('Úsuario cadastrado com sucesso')
             cont = 1 
     
-        
+def registrarCampeonato():
+    cont1 = 0
+    while cont1 == 0:
+        codigo_campeonato = random.randrange(1001, 9999)
+        cod_Campeonato = input(f'Insira o código do campeonato [{codigo_campeonato}] -> ')
+
+        if cod_Campeonato.isnumeric() == True:
+            bd1.cursor.execute("select cod_campeonato from campeonatos where cod_campeonato = %s", (cod_Campeonato,))
+            bdcod_camp = bd1.cursor.rowcount
+            if int(cod_Campeonato) != int(codigo_campeonato) or bdcod_camp > 0:
+                print('Código errado ou já usado, digite novamente.')
+                continue
+            elif int(cod_Campeonato) == int(codigo_campeonato):
+                print('Correto')
+                cont1 = 1
+        else:
+            print('Apenas números!')
+            continue
+    nome_Campeonato = input('Insira o nome do campeonato -> ')
+    data_inicio = input('Insira a data de ínicio [dd/mm/yyyy] -> ')
+    data_fim = input('Insira a data final [dd/mm/yyyy] -> ')
+    categoria = input('Insira a categoria -> ')
+
+    bd1.cursor.execute("insert into campeonatos(cod_campeonato,nome,data_inicio,data_fim,categoria) values(%s,%s,%s,%s,%s)",(cod_Campeonato,nome_Campeonato,data_inicio,data_fim,categoria))
+    bd1.connection.commit()
+    print('Campeonato registrado.')
+           
+def VerCamp():
+    cod_camps = int(input('Insira o código do campeonato -> '))
+
+    bd1.cursor.execute("select cod_campeonato from campeonatos where cod_campeonato = %s", (cod_camps,))
+    bdcod_camps = bd1.cursor.rowcount
+
+    if bdcod_camps > 0:
+        print('verificado')
+    else:
+        print('Campeonato inexistente.')
 
 
 def Entrar():
@@ -53,12 +90,15 @@ def Entrar():
 
         bd1.cursor.execute("SELECT usuario,senha FROM usuarios WHERE usuario=%s AND senha=%s",(usuario, senha))
         numRow = bd1.cursor.rowcount
-        print('-> Sair\n-> Cadastrar time\n-> Times registrados\n-> Configurações\n-> Vincular telefone')
+        print('-> Sair\n-> Times registrados\n-> Configurações\n-> Vincular telefone')
         ops = input()
+        if ops == 'sair':
+            pop = 1
         if numRow > 0:
             #usuário logado
             
-            
+
+           
         
             # if ops == 'configurações':
             #     bd1.cursor.execute("SELECT email FROM usuarios WHERE email = %s",(recebeEmail,))
@@ -67,10 +107,15 @@ def Entrar():
 
             if ops == 'vincular telefone':
                 back = 0
+                
                 while back == 0:
                     tel = input('Digite o número [+DDD][8212345678] -> +')
                     
-                    if tel.isnumeric() == True: 
+                    if len(tel) > 13:
+                        print('Número passou do limite [11 Números]')
+                        continue
+
+                    if tel.isnumeric() == True : 
                         bd1.cursor.execute("select telefone from usuarios where usuario = %s",(usuario,))  
                         pegar = bd1.cursor.fetchall()
                         if pegar[0] == 0:
@@ -89,22 +134,18 @@ def Entrar():
                         print('Apenas números!')
                         continue
 
+            
 
         else:
             print("Usuário ou senha incorretos!\n")
             continue
-    
-    #mostra email
-    
-    #aqui tu bota o q tu quiser, menu sla
-
 
 
 conti = 0
 
 
 while conti == 0:
-    print('-> Ver times\n-> Cadastrar\n-> Categorias\n-> Criar Conta\n-> Entrar')
+    print('-> Entrar\n-> Criar Conta\n-> Plataformas\n-> Registrar campeonato\n-> Ver campeonatos [ativos]')
     opc = input()
 
     if opc == 'criar conta':
@@ -112,3 +153,11 @@ while conti == 0:
         continue
     elif opc == 'entrar':
         Entrar()
+        continue
+    elif opc == 'registrar campeonato':
+        registrarCampeonato()
+        continue
+    elif opc == 'ver campeonatos':
+        VerCamp()
+        exit()
+        

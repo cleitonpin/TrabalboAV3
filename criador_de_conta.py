6308175c00448c3ac4983fd38d0047a75124eece
Elicácio C. de Farias <elicaciocardoso@gmail.com>
2019-11-08 14:49:02 -0300

    criei o pacote validador com uma função para validar a data de nascimento e separei o codigo de criação de conta em um módulo

---
+++ criador_de_conta.py
@@ -0,0 +1,70 @@
+# -*- utf-8 -*-
+
+import hashlib
+import datetime
+import time
+from validador import data
+
+def cria():
+
+    continua = 1
+    while continua == 1:
+
+        telefone = 0
+        data_nascimento = input('Insira sua data de nascimento [+14] [dd/mm/yyyy]: ')
+
+        if data.valida(data_nascimento):
+            print('ERROR: FORMATO DA DATA INVÁLIDO')
+
+        usuario = input('Insira seu usuario: ')
+        senha = input('Insira sua senha: ')
+        email = input('Insira um e-mail válido: ')
+        nick = input('Insira seu nick: ')
+        
+        hash = hashlib.md5(str(senha).encode('utf-8'))
+        senhaMD5 = hash.hexdigest()
+        
+        verificaremail = (email.find('@'),email.find('.com')) 
+
+        str_date = '01/01/2005'
+        date = datetime.strptime(str_date, '%d/%m/%Y')
+        date2 = datetime.strptime(data_nascimento, '%d/%m/%Y')
+
+        if date2 > date:
+            print('Apenas maiores de 14 anos.')
+            time.sleep(5)
+            continue
+        if verificaremail[0] == -1:
+            print("Erro: email inválido [ausência de: '@']\n")
+            time.sleep(5)
+            continue
+        if verificaremail[1] == -1:
+            print("Erro: email inválido [ausência de: '.com/.com.br/...']\n")
+            time.sleep(5)
+            continue
+        if len(usuario) < 4:
+            print('Número de caracteres insuficiente[mínimo 4]\n')
+            time.sleep(5)
+            continue
+        elif len(usuario) > 12:
+            print('Número de caracteres acima do indicado[máximo de 12]\n')
+            time.sleep(5)
+            continue
+
+        bd1.cursor.execute("select usuario from usuarios where usuario = %s", (usuario,))
+        rowUsuario = bd1.cursor.rowcount 
+        bd1.cursor.execute("select nick from usuarios where nick = %s", (nick,))
+        rowNick = bd1.cursor.rowcount 
+
+        row= (rowUsuario, rowNick)
+        if row[0] > 0 or row[1] > 0:
+            if row[0] > 0:
+                print('ERROR: Usuário já existe')
+            if row[1] > 0:
+                print('ERROR: Nick já existe')      
+        else: 
+            bd1.cursor.execute("insert into usuarios(telefone,email,usuario,nick,data_nascimento,senha) values(%s,%s,%s,%s,%s,%s)",(telefone,email,usuario,nick,data_nascimento,senha))
+            bd1.connection.commit()
+            print('Úsuario cadastrado com sucesso')
+            time.sleep(5)
+            continua = 0 
---
+++ validador/data.py
@@ -0,0 +1,4 @@
+import time
+
+def valida(data):
+    return data.find('/') == -1 or len(data) < 10

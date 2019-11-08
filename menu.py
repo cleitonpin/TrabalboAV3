46f0da57b77fa5c1a72384c46e90cdf2cf14937c
Elicácio C. de Farias <elicaciocardoso@gmail.com>
2019-11-08 10:36:47 -0300

    coloquei o codigo que mostra o menu na função menu

---
+++ menu.py
@@ -0,0 +1,11 @@
+def show():
+
+    menu = """
+        color.RED+"#"*53+color.END
+        color.RED+"#                                                   #"+color.END
+        color.RED+"#"+color.END+color.DARKCYAN+"RifsPopo"+color.END+color.RED+"#"+color.END
+        color.RED+"#                                                   #"+color.END
+        color.RED+"#"*53+color.END
+    """
+    
+    print(menu)
--- trabson.py
+++ trabson.py
@@ -1,29 +1,25 @@
-import bd1
+# import bd1
 import random
 import time
 import os 
 from datetime import datetime
 import hashlib
 import sys
+import menu
 
 class color:
-   PURPLE = '\033[95m'
-   CYAN = '\033[96m'
-   DARKCYAN = '\033[36m'
-   BLUE = '\033[94m'
-   GREEN = '\033[92m'
-   YELLOW = '\033[93m'
-   RED = '\033[91m'
-   BOLD = '\033[1m'
-   UNDERLINE = '\033[4m'
-   END = '\033[0m'
-def menu():
-    print(color.RED+"#"*53+color.END)
-    print(color.RED+"#                                                   #"+color.END)
-    print(color.RED+"#"+color.END+color.DARKCYAN+"                      RifsPopo                     "+color.END+color.RED+"#"+color.END)
-    print(color.RED+"#                                                   #"+color.END)
-    print(color.RED+"#"*53+color.END)
-   
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
+
+menu.show()
 
 def cls():
   os.system('cls' if os.name == 'nt' else 'clear')

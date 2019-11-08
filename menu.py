8ca76ba4ca4a1b98b8d127f1d16ff37cf709d71f
Elicácio C. de Farias <elicaciocardoso@gmail.com>
2019-11-08 14:21:31 -0300

    refatorei o menu

--- menu.py
+++ menu.py
@@ -1,11 +1,29 @@
-def show():
-
-    menu = """
-        color.RED+"#"*53+color.END
-        color.RED+"#                                                   #"+color.END
-        color.RED+"#"+color.END+color.DARKCYAN+"RifsPopo"+color.END+color.RED+"#"+color.END
-        color.RED+"#                                                   #"+color.END
-        color.RED+"#"*53+color.END
+def show(color):
+    """Mostra o menu
     """
+    mensagem = color.DARKCYAN + 'Não tem uma conta? Crie agora, basta digitar 2'
+    espaco = int(((80 - len(mensagem)) / 2)  + 2)
+    cor_vermelha = color.RED
+    jogo_da_velha = "#"
+
+    linha_de_fora = cor_vermelha + jogo_da_velha * 80
+    linha_interna = cor_vermelha + jogo_da_velha + " " * 78 + jogo_da_velha
+    linha_do_meio = "{COR}{JOGO_DA_VELHA}{ESPACO}{MENSAGEM}{ESPACO}{COR}{JOGO_DA_VELHA}".format(
+        COR = cor_vermelha,
+        JOGO_DA_VELHA = jogo_da_velha,
+        ESPACO = " "* espaco,
+        MENSAGEM = mensagem
+    )
     
-    print(menu)
+    print("""
+        {LINHA_DE_FORA}
+        {LINHA_INTERNA}
+        {LINHA_DO_MEIO}
+        {LINHA_INTERNA}
+        {LINHA_DE_FORA}
+    """.format(
+        LINHA_DE_FORA = linha_de_fora,
+        LINHA_INTERNA = linha_interna,
+        LINHA_DO_MEIO = linha_do_meio
+        )
+    )
